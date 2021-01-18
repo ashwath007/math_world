f540be4303b38fa14b2bc9e242f4ddba2dcf86b9
vicky <srivicky2000@gmail.com>
2021-01-18 23:39:08 +0530

    Condition Propd

--- Probablity/conditionalprobablity.py
+++ Probablity/conditionalprobablity.py
@@ -2,12 +2,21 @@ import math as m
 import os
 import sys
 
-class conditionprob:
-    def ___init__(self,x,y,prop):
+class conditionprob(): //Parent Class
+    def __init__(self,x,y,prop):
         self.x = x
         self.y = y
         self.prop = prop
+    def cal(self):
+        return self.x*self.y
+
+class poppass(conditionprob): //Children Class
+    def ok():
+        print("END")
 
 
-
-
+e = conditionprob(10,22,90.89)
+print(e.__dict__)
+print(e.cal())
+ob = poppass(10,222,232.89)
+print(ob.cal())
