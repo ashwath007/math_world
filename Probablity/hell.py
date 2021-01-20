f2026df6beb50a0d8905ac883319535a666ca1b2
vicky <srivicky2000@gmail.com>
2021-01-20 13:19:30 +0530

    Sum_of_error RMSE and MSE

--- Probablity/hell.py
+++ Probablity/hell.py
@@ -1,18 +1,29 @@
-
+from math import sqrt
 
 
 class ev():
-    def __init__(self,y_pred,y,status=True):
+    def __init__(self,y_pred,y,*RMSE):
         self.y_pred = y_pred
         self.y = y
+        self.key = RMSE
+        
     def MSE(self):
         sum_ar = []
-        if(len(self.y_pred) == len(self.y)):
-            for i in range(len(self.y_pred)):
-                sum_ar.append((self.y_pred[i] - self.y[i])**2)
-            return sum(sum_ar)/len(self.y_pred)
+        print(self.key[0])
+        if(self.key[0]):
+            if(len(self.y_pred) == len(self.y)):
+                for i in range(len(self.y_pred)):
+                    sum_ar.append((self.y_pred[i] - self.y[i])**2)
+                return sum(sum_ar)/len(self.y_pred)
+            else:
+                raise Exception("Provide y^ and y of same dimentions") 
         else:
-            return "Provide y^ and y of same dimentions"
+            if(len(self.y_pred) == len(self.y)):
+                for i in range(len(self.y_pred)):
+                    sum_ar.append((self.y_pred[i] - self.y[i])**2)
+                return sqrt(sum(sum_ar)/len(self.y_pred))
+            else:
+                raise Exception("Provide y^ and y of same dimentions") 
 
 
 
--- Probablity/test.py
+++ Probablity/test.py
@@ -3,6 +3,6 @@ import hell as H
 
 y_true = [3, -0.5, 2, 7]
 y_pred = [2.5, 0.0, 2, 8]
-
-c = H.ev(y_true,y_pred)
+s=False
+c = H.ev(y_true,y_pred,s)
 print(c.MSE())
