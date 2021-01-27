b6c5f1b2126522105d4c9515b7bfdfe734c70baf
vicky <srivicky2000@gmail.com>
2021-01-27 15:12:04 +0530

    Manhattan distance

--- Probablity/distance.py
+++ Probablity/distance.py
@@ -12,9 +12,15 @@ class distance():
             return sqrt((X[0]-Y[0])**2 + (X[1]-Y[1])**2)
         else:
             return "Please provide correct dimention"
+    def manhattan(self):
+        if len(self.x) ==2:
+            X = self.x[0]
+            Y = self.x[1]
+            return (abs((X[0]-Y[0])) + abs((X[1]-Y[1])))
+
 
 #Test Class
-'''dis = distance([[3,1],[2,1]])
-dis1 = distance([[2,1],[2,3]])
+dis = distance([[3,1],[2,1]])
+dis1 = distance([[2, 3], [5, 6]])
 print(dis.euclidean())
-print(dis1.euclidean())'''
+print(dis1.manhattan())
