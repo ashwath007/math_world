b9c37d47cd81336c431e2b5c59e3e361650cf866
vicky <srivicky2000@gmail.com>
2021-01-27 19:41:39 +0530

    Hamming dtstance added

--- Probablity/distance.py
+++ Probablity/distance.py
@@ -17,13 +17,7 @@ class distance():
             X = self.x[0]
             Y = self.x[1]
             return (abs((X[0]-Y[0])) + abs((X[1]-Y[1])))
-def help(self):
-    print("Help section for Distance")
-    return('''
-        1. Euclidean distance - 
-                    Use euclidean() function with parameter [[cord1,cord2]]
-                                    
-    ''')
+
 def manhattan2d(a,b):
     return sum(abs(e1-e2) for e1,e2 in zip(a,b))
 def euclidean2d(a,b):
@@ -31,18 +25,17 @@ def euclidean2d(a,b):
         return sqrt(sum((e1-e2)**2 for e1, e2 in zip(a,b)))   
     else:
         return "Try changing the dimention or use euclidean([[],[]]) function"
-#Test Class
-# dis = distance([[10,12],[20,24],[15,18],[10,8],[5,7]])
-# dis1 = distance([[2, 3], [5, 6]])
-# dd = distance()
-# row1 = [10, 20, 15, 10, 5]
-# row2 = [12, 24, 18, 8, 7]
-# print(dd.euclidean2d(row1, row2))
-# print(dd.manhattan2d(row1, row2))
 
-# print(dd.help())
-
-# print(dis1.manhattan())
+def hamming(a,b):
+    return sum(abs(a-b) for a,b in zip(a,b))/len(a)
 
 
-# Todo : return sqrt(sum((e1-e2)**2 for e1, e2 in zip(a,b)))
+
+
+def help(self):
+    print("Help section for Distance")
+    return('''
+        1. Euclidean distance - 
+                    Use euclidean() function with parameter [[cord1,cord2]]
+                                    
+    ''')
--- Probablity/test.py
+++ Probablity/test.py
@@ -1,18 +1,38 @@
 import hell as H
 # import distance as d
-from distance import manhattan2d,euclidean2d
+from distance import manhattan2d,euclidean2d,hamming
 
 y_true = [3, -0.5, 2, 7]
 y_pred = [2.5, 0.0, 2, 8]
 s=False
 c = H.ev(y_true,y_pred,s)
-print(c.MSE())
+# print(c.MSE())
 
 
 # dis = d.distance([[10,12],[20,24],[15,18],[10,8],[5,7]])
 # dis1 = d.distance([[2, 3], [5, 6]])
 # dd = d.distance()
-row1 = [10, 20, 15, 10, 5]
-row2 = [12, 24, 18, 8, 7]
-print(euclidean2d(row1, row2))
+# row1 = [10, 20, 15, 10, 5]
+# row2 = [12, 24, 18, 8, 7]
+row1 = [0, 0, 0, 0, 0, 1]
+row2 = [0, 0, 0, 0, 1, 0]
+# print(euclidean2d(row1, row2))
+print(hamming(row1, row2))
+
 # print(manhattan2d(row1, row2))
+
+#Test Class
+# dis = distance([[10,12],[20,24],[15,18],[10,8],[5,7]])
+# dis1 = distance([[2, 3], [5, 6]])
+# dd = distance()
+# row1 = [10, 20, 15, 10, 5]
+# row2 = [12, 24, 18, 8, 7]
+# print(dd.euclidean2d(row1, row2))
+# print(dd.manhattan2d(row1, row2))
+
+# print(dd.help())
+
+# print(dis1.manhattan())
+
+
+# Todo : return sqrt(sum((e1-e2)**2 for e1, e2 in zip(a,b)))
