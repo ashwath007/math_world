65efdf07d2bb408de3500be9bd325ad3f93cc4fd
vicky <srivicky2000@gmail.com>
2021-01-27 17:48:41 +0530

    Manhattan distance 2D

--- Probablity/distance.py
+++ Probablity/distance.py
@@ -3,8 +3,11 @@ from math import sqrt
 
 
 class distance():
-    def __init__(self,x):
+    def __init__(self,x=''):
         self.x = x
+    def euclidean2d(self,a,b):
+        return sqrt(sum((e1-e2)**2 for e1, e2 in zip(a,b)))   
+
     def euclidean(self):
         if len(self.x) ==2:
             X = self.x[0]
@@ -20,7 +23,13 @@ class distance():
 
 
 #Test Class
-dis = distance([[3,1],[2,1]])
+dis = distance([[10,12],[20,24],[15,18],[10,8],[5,7]])
 dis1 = distance([[2, 3], [5, 6]])
-print(dis.euclidean())
-print(dis1.manhattan())
+dd = distance()
+row1 = [10, 20, 15, 10, 5]
+row2 = [12, 24, 18, 8, 7]
+print(dd.euclidean2d(row1, row2))
+# print(dis1.manhattan())
+
+
+# Todo : return sqrt(sum((e1-e2)**2 for e1, e2 in zip(a,b)))
--- Probablity/test.py
+++ Probablity/test.py
@@ -1,8 +1,11 @@
-import hell as H
+# import hell as H
 
 
-y_true = [3, -0.5, 2, 7]
-y_pred = [2.5, 0.0, 2, 8]
-s=False
-c = H.ev(y_true,y_pred,s)
-print(c.MSE())
+# y_true = [3, -0.5, 2, 7]
+# y_pred = [2.5, 0.0, 2, 8]
+# s=False
+
+ll=[[2,3],[4,5],[8,9]]
+print(ll[0][0])
+# c = H.ev(y_true,y_pred,s)
+# print(c.MSE())
