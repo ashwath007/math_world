6fbe467159623439503e27b2539b5e6c05111b5c
vicky <srivicky2000@gmail.com>
2021-01-27 14:47:59 +0530

    Distance 1

--- Probablity/distance.py
+++ Probablity/distance.py
@@ -1,9 +1,18 @@
-
+from math import sqrt
 
 
 
 class distance():
-    def __init__(self,x,y):
-        self .x = x
-        self.y = y
-    def 
+    def __init__(self,x):
+        self.x = x
+    def euclidean():
+        if len(self.x) ==2:
+            X = self.x[0]
+            Y = self.x[1]
+            return sqrt((X[0]-Y[0])**2 + (X[1]-Y[1])**2)
+        else:
+            return "Please provide correct dimention"
+
+
+dis = distance([[0,1],[1,1]])
+print(dis.euclidean())
